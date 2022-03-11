e932e91fffc87d74dca8823d351fa4fe6fa5cf47
pedeli123 <101356642+pedeli123@users.noreply.github.com>
2022-03-10 20:56:36 -0800

    Update script.js

--- script.js
+++ script.js
@@ -240,7 +240,7 @@ var initMap = function() {
 
 
   L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+//    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
     subdomains: 'abcd',
     maxZoom: 19
   }).addTo(map);
