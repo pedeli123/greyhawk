50c140bdbe1d6cfe78e546d2e087296913c76b54
pedeli123 <101356642+pedeli123@users.noreply.github.com>
2022-03-13 12:51:57 -0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 var data = {};
 var groups = {};
+var finder; //ADDED VARIABLE FOR FINDER
 var map;
 
 /*
@@ -266,13 +267,13 @@ var initMap = function() {
 // ----->END [GREYHAWK MAP]
 	
 // ----->ADD Coordinate Finder
-//  var finder = L.finder([0, 0], {
-//	draggable: true,
-//  }).addTo(map);
-//  finder.bindPopup('LatLng Marker').openPopup();
-//  finder.on('dragend', function(e) {
-//	finder.getPopup().setContent(finder.getLatLng().toString()).openOn(map);
-//  });
+  finder = L.finder([0, 0], {
+	draggable: true,
+  }).addTo(map);
+  finder.bindPopup('LatLng Marker').openPopup();
+  finder.on('dragend', function(e) {
+	finder.getPopup().setContent(finder.getLatLng().toString()).openOn(map);
+  });
 // ----->END Coordinate Finder
   
   loadData(dataLocation);
