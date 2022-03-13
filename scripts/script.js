13c8bac7fc09e9502e4ad2a6b22d9d9bfb6a7453
pedeli123 <101356642+pedeli123@users.noreply.github.com>
2022-03-13 12:22:05 -0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -155,7 +155,7 @@ var addMarkers = function(data) {
         placeInfo: d
       },
     ).on('click', function(e) {
-      map.flyTo(this._latlng, 8); // ----->CHANGED FROM 11 ZOOM TO 8
+      map.flyTo(this._latlng, 7); // ----->CHANGED FROM 11 ZOOM TO 7
       updateSidebar(this);
     });
 
@@ -256,7 +256,7 @@ var initMap = function() {
 	continuousWorld: false, //-->ADDED
 	noWrap: true, //-->ADDED
 	minZoom: 2, //-->ADDED
-	maxZoom: 9, //-->ADDED
+	maxZoom: 8, //-->ADDED
 // 	subdomains: 'abcd',
 //	maxZoom: 19
   }).addTo(map);  
